298cc2982e157e2e8ac064b7a1304172166f5792
Tilak Raj <tilakrajinfo@users.noreply.github.com>
2014-06-26 01:25:32 +0530

    Update taking_Input_from_keyBoard_Using_Scanner.java

--- taking_Input_from_keyBoard_Using_Scanner.java
+++ taking_Input_from_keyBoard_Using_Scanner.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
-public class InputFromKeyBoard {
+public class taking_Input_from_keyBoard_Using_Scanner {
 	public static void main(String args[])
 
 	{
 		Scanner keyboard=new Scanner(System.in); 
 		
-		System.out.println("Input you name");
+		System.out.println("Input you name"); 
 		String abc=keyboard.nextLine();
 		
 		System.out.println(abc);
